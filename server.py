a384f7786a21b8e0b40e00b4c4dd40c6c3358ceb
Abdlrahman-Khaled <202308169@stu.uob.edu.bh>
2025-12-13 16:33:14 +0300

    importing the libraries and define the main veriavles

---
+++ server.py
@@ -0,0 +1,10 @@
+import socket
+import threading
+import json
+import urllib.request
+import urllib.error
+
+API_key = "da2a3228d31f45f3809df6be622c2749" 
+host = "127.0.0.1"
+port = 5000
+group_id ="GA18"
