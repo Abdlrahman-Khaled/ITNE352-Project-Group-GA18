faa54e5e9622196e6b43d89800df39fb4bc85b32
Ali Jaber <alooy268@gmail.com>
2025-12-15 14:26:44 +0300

    Add Headlines and Sources muny

--- client.py
+++ client.py
@@ -8,7 +8,7 @@ Languages=["ar", "en" ]
 Categories=["business", "general", "health", "science", "sports", "technology" ]
 
 def display_Main_Menu():
-    print("\n"+"*"*15+"The Main Menu"+"*"*15)
+    print("\n"+"*"*15+" The Main Menu "+"*"*15)
     print("""1.Search headlines 
 2.List of Sources
 3.Quit """)
@@ -16,4 +16,25 @@ def display_Main_Menu():
 
 
 
-display_Main_Menu()
+def display_Headlines_Menu():
+    print("\n"+"*"*15+" The Headlines Menu "+"*"*15)
+    print("""1.Search for keywords   
+2.Search by category     
+3.Search by country    
+4.List all new headlines  
+5.Back to the main menu    """)
+    print("*"*50)
+
+
+
+def display_Sources_Menu():
+    print("\n"+"*"*15+" The Sources Menu "+"*"*15)
+    print("""1.Search by category   
+2.Search by country     
+3.Search by language    
+4.List all 
+5.Back to the main menu    """)
+
+
+display_Headlines_Menu()
+display_Sources_Menu()
